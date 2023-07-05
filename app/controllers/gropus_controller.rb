67a84b2272ef589999e467ea8be0783d36ac9bbe
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-05 21:30:45 +0300

    Added group_controller.rb file

---
+++ app/controllers/gropus_controller.rb
@@ -0,0 +1,64 @@
+class GroupsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @group = Group.where(user_id: current_user.id)
+    end
+
+    def show
+        @group = Group.find(group.id)
+    end
+
+    def new
+        @group = Group.new
+    end
+
+    def edit; end
+
+    def create
+        @group = Group.new(group_params.merge(user_id: current_user.id))
+
+
+        respond_to do |format|
+            if @group.save
+                format.html { redirect_to group__path, notice: 'Group was successfully created.' }
+                format.json { render :show, status: :created, location: @group }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @group.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @group.update(group_params)
+                format.html { redirect_to group_path, notice: 'Group was successfully updated.' }
+                format.json { render :show, status: :ok, location: @group }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @group.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @group.destroy
+
+        respond_to do |format|
+
+            format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+            format.json { head :no_content }
+        end
+    end
+
+    private
+
+    def set_group
+        @group = Group.find(params[:id])
+    end
+
+    def group_params
+        params.require(:group).permit(:name, :icon)
+    end
+end
